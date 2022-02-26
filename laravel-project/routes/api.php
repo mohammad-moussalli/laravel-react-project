130046315c903121793f0992714b7b73758d9f34
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-26 22:32:37 +0200

    Add update-data route

--- laravel-project/routes/api.php
+++ laravel-project/routes/api.php
@@ -20,5 +20,7 @@ Route::group([
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);  
+    Route::post('/update-data', [AuthController::class, 'updateData']);    
+  
 });
