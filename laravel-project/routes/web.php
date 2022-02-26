7860a0516c721a896e00bf4cd0038ff2a588058e
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-26 14:04:33 +0200

    Move MainController to new folder and add routes

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, "index"])->name("web:index");
+Route::get('/signin', [MainController::class, "index"])->name("web:signin");
+Route::get('/signup', [MainController::class, "index"])->name("web:signup");
+Route::get('/dashboard', [MainController::class, "index"])->name("web:dashboard");
