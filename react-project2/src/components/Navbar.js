4adde6a399f3f4177bb23db97da6c372dfcc34ed
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-27 23:58:45 +0200

    Edit link from href into to

--- react-project2/src/components/Navbar.js
+++ react-project2/src/components/Navbar.js
@@ -3,18 +3,17 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return ( 
         <nav className="Navbar">
-            <h1>Navbar</h1>
+            <h1>Laravel/React Project</h1>
             <div className="links">
-                <Link href="/">Landing</Link>
-                <Link href="/login-signup"style={{ 
+                <Link to="/">Homepage</Link>
+                <Link to="/login-signup"style={{ 
                     color: 'white', 
                     backgroundColor: '#f1356d',
                     borderRadius: '8px' 
                 }}>Login/Signup</Link>
-                <Link href="/dashboard">Dashboard</Link>
-                <Link href="/about-us">About Us</Link>
-                <Link href="/services">Services</Link>
-
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/about-us">About Us</Link>
+                <Link to="/services">Services</Link>
             </div>
         </nav>
      );
