dbdf75859768a7c6416d3a2defb38e543b4ea9bd
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-28 18:33:49 +0200

    Add useState function to clear textfield

--- react-project2/src/components/ContactUs.js
+++ react-project2/src/components/ContactUs.js
@@ -1,16 +1,18 @@
 import { Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
 
 
 const ContactUs = () => {
+    const [val, setVal] = useState();
+
     return (
         <Box sx={{ flexDirection: 'column', display: 'flex', justifyContent: 'space-evenly', alignItems: 'center' }}>…
             <h2>Contact Us</h2>
-            <TextField id="outlined-basic" label="Name" variant="outlined" style={{margin: "5px"}} />
-            <TextField id="outlined-basic" label="Email" variant="outlined" style={{margin: "5px"}} />
-            <TextField id="outlined-basic" label="Message" variant="outlined" style={{margin: "5px"}} />
-            <Button variant="text" style={{color: 'white', backgroundColor: '#f1356d', borderRadius: '8px'}} >Submit</Button>
-            
+            <TextField id="outlined-basic" label="Name" variant="outlined" style={{margin: "5px"}} value={val}/>
+            <TextField id="outlined-basic" label="Email" variant="outlined" style={{margin: "5px"}} value={val}/>
+            <TextField id="outlined-basic" label="Message" variant="outlined" style={{margin: "5px"}} value={val}/>
+            <Button variant="text" style={{color: 'white', backgroundColor: '#f1356d', borderRadius: '8px'}} onClick={() => setVal(() => "")}>Submit</Button>
         </Box>
     );
   }
