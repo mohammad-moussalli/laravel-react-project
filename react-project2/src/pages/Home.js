18a35399562bbb70960101f60e18aa750b071682
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-28 17:39:08 +0200

    Add ContactUs component to Landing page

--- react-project2/src/pages/Home.js
+++ react-project2/src/pages/Home.js
@@ -1,15 +1,10 @@
+import ContactUs from "../components/ContactUs";
+
 const Home = () => {
 
   return (
-    <div className="home">
-      <h2>Contact Us</h2>
-      <h4>Name</h4>
-      <input type="text" />
-      <h4>Email</h4>
-      <input type="text" />
-      <h4>Message</h4>
-      <input type="text" />
-      <button>Submit</button>
+    <div className="contact-us">
+      <ContactUs />
     </div>
   );
 }
