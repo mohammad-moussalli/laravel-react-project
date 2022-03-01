44e2c7645a863d3c64926c297a07f3baf67d6979
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-01 02:53:02 +0200

    Edit Styling

--- react-project2/src/components/ContactUs.js
+++ react-project2/src/components/ContactUs.js
@@ -13,16 +13,17 @@ const ContactUs = () => {
             justifyContent: 'space-evenly', 
             alignItems: 'center', 
             borderColor: '#333', 
-            maxWidth:'300px', 
+            minWidth:'550px', 
+            minHeight:'550px',
             border:'solid', 
             borderRadius:'10px',
             padding: '10px',
             backgroundColor: 'wheat',
             }}>
             <h2>Contact Us</h2>
-            <TextField id="outlined-basic" label="Name" variant="outlined" style={{margin: "10px", backgroundColor:'white', borderRadius:'5px'}} value={val}/>
-            <TextField id="outlined-basic" label="Email" variant="outlined" style={{margin: "10px", backgroundColor:'white', borderRadius:'5px'}} value={val}/>
-            <TextField id="outlined-multiline-static" label="Message" multiline rows={5} variant="outlined" style={{margin: "10px", backgroundColor:'white', borderRadius:'5px'}} value={val}/>
+            <TextField id="outlined-basic" label="Name" variant="outlined" style={{margin: "10px", backgroundColor:'white', borderRadius:'5px', minWidth: 530}} value={val}/>
+            <TextField id="outlined-basic" label="Email" variant="outlined" style={{margin: "10px", backgroundColor:'white', borderRadius:'5px', minWidth: 530}} value={val}/>
+            <TextField id="outlined-multiline-static" label="Message" multiline rows={5} variant="outlined" style={{margin: "10px", backgroundColor:'white', borderRadius:'5px', minWidth: 530}} value={val}/>
             <Button className="Button" variant="text" style={{backgroundColor:'wheat', borderRadius:'10px', color:'#333'}} onClick={() => setVal(() => "")}>Submit</Button>
         </Box>
     );
