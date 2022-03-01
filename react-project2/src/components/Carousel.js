89065e15c5d8f82e8a07412b8a9cb6035adca386
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-01 02:51:01 +0200

    Implement Carousel Component

---
+++ react-project2/src/components/Carousel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
+
+const ImageCarousel = () => {
+  const images = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1].map((number) => ({
+    src: `https://placedog.net/${number}00/${number}00?id=${number}`
+  }));
+
+  return (
+    <Carousel images={images} style={{ height: 600, width: 600}} />
+  );
+};
+
+export default ImageCarousel;
