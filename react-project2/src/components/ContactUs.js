5b75b882f001bfefb7247d3ecabc04e1a81a1c6d
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-02 18:27:57 +0200

    Add export constants for style

--- react-project2/src/components/ContactUs.js
+++ react-project2/src/components/ContactUs.js
@@ -1,6 +1,7 @@
 import { Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import { useState } from "react";
+import { BUTTON_COLOR, BUTTON_RADIUS, TEXTFIELD_COLOR, TEXTFIELD_MARGIN, TEXTFIELD_RADIUS, TEXT_COLOR } from "../constants/styles";
 
 
 const ContactUs = () => {
@@ -21,10 +22,10 @@ const ContactUs = () => {
             backgroundColor: 'wheat',
             }}>
             <h2>Contact Us</h2>
-            <TextField id="outlined-basic" label="Name" variant="outlined" style={{margin: "10px", backgroundColor:'white', borderRadius:'5px', minWidth: 530}} value={val}/>
-            <TextField id="outlined-basic" label="Email" variant="outlined" style={{margin: "10px", backgroundColor:'white', borderRadius:'5px', minWidth: 530}} value={val}/>
-            <TextField id="outlined-multiline-static" label="Message" multiline rows={5} variant="outlined" style={{margin: "10px", backgroundColor:'white', borderRadius:'5px', minWidth: 530}} value={val}/>
-            <Button className="Button" variant="text" style={{backgroundColor:'wheat', borderRadius:'10px', color:'#333'}} onClick={() => setVal(() => "")}>Submit</Button>
+            <TextField id="outlined-basic" label="Name" variant="outlined" style={{margin: TEXTFIELD_MARGIN, backgroundColor: TEXTFIELD_COLOR, borderRadius: TEXTFIELD_RADIUS, minWidth: 530}} value={val}/>
+            <TextField id="outlined-basic" label="Email" variant="outlined" style={{margin: TEXTFIELD_MARGIN, backgroundColor: TEXTFIELD_COLOR, borderRadius: TEXTFIELD_RADIUS, minWidth: 530}} value={val}/>
+            <TextField id="outlined-multiline-static" label="Message" multiline rows={5} variant="outlined" style={{margin: TEXTFIELD_MARGIN, backgroundColor: TEXTFIELD_COLOR, borderRadius: TEXTFIELD_RADIUS, minWidth: 530}} value={val}/>
+            <Button className="Button" variant="text" style={{backgroundColor:BUTTON_COLOR, borderRadius:BUTTON_RADIUS, color: TEXT_COLOR}} onClick={() => setVal(() => "")}>Submit</Button>
         </Box>
     );
   }
