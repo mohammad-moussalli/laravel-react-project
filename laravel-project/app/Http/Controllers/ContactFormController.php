f98404a8668acea67cd6e48aa678837ad1510f85
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-02 13:00:11 +0200

    Implement ContactUs Api

---
+++ laravel-project/app/Http/Controllers/ContactFormController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\Models\Contact;
+
+class ContactFormController extends Controller
+{
+    // Store Contact Form data
+    public function contactForm(Request $request) {
+
+        // Form validation
+            $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required'
+            ]);
+            if ($validator->fails()) { 
+        return response()->json(['error'=>$validator->errors()], 401);            
+    }
+        //  Store data in database
+        Contact::create($request->all());
+        return response()->json(['success' => 'The email has been sent.']);
+    }
+}
