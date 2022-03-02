ebca40fabeceb5c8f1ebf36dd6a25cc051fab65c
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-02 18:25:19 +0200

    Edit Navbar and add Logout Button

--- react-project2/src/components/Navbar.js
+++ react-project2/src/components/Navbar.js
@@ -10,98 +10,116 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import { Link } from "react-router-dom";   
+import { useHistory } from "react-router-dom";
+import { FILL_COLOR } from '../constants/styles';
 
+const Navbar = () => {
 
 const Homepage = <Link to="/" style={{ textDecoration : "None",borderRadius: '8px' }}>Homepage</Link>;
-const LoginSignup = <Link to="/login-signup"style={{ textDecoration : "None",borderRadius: '8px' }}>Login/Signup</Link>;
 const Dashboard = <Link to="/dashboard"style={{ textDecoration : "None",borderRadius: '8px' }}>Dashboard</Link>;
 const AboutUs = <Link to="/about-us"style={{ textDecoration : "None",borderRadius: '8px' }}>About Us</Link>;
 const Services = <Link to="/services"style={{ textDecoration : "None",borderRadius: '8px' }}>Services</Link>;
-const pages = [Homepage, LoginSignup, Dashboard, AboutUs, Services];
+const LoginSignup = <Link to="/login-signup"style={{ textDecoration : "None",borderRadius: '8px' }}>Login/Signup</Link>;
+const Logout = <Button  className='Button' variant="text" style={{backgroundColor:'wheat', borderRadius:'10px', color:'#333'}} onClick ={() => clearLocalStorage()}>Logout</Button>
 
 
-const Navbar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+let pages = [];
+let settings = [];
+if (localStorage.getItem('token') !== null) {
+    pages = [Homepage, Dashboard, AboutUs, Services];
+    settings = [Logout];
+}else{
+    pages = [Homepage, AboutUs, Services];
+    settings = [LoginSignup];
+}
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
- 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+const history = useHistory();
+function clearLocalStorage(){
+    localStorage.clear();
+    history.push('/login-signup');
+}
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+  
+    const handleOpenNavMenu = (event) => {
+      setAnchorElNav(event.currentTarget);
+    };
+  
+    const handleCloseNavMenu = () => {
+      setAnchorElNav(null);
+    };
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl" style={{backgroundColor:'wheat'}}>
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-          >
-            <h3>Laravel/React Project</h3>
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
+    return (
+      <AppBar position="static">
+        <Container maxWidth="xl" style={{backgroundColor:FILL_COLOR}}>
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-            >
-            <h3>Laravel/React Project</h3>
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+              <h3>Laravel/React Project</h3>
+            </Typography>
+  
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
               >
-                {page}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              <h3>Laravel/React Project</h3>
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+                {settings}
+          </Toolbar>
+        </Container>
+      </AppBar>
   );
 };
 export default Navbar;
