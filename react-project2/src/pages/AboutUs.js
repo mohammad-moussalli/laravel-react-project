5930a92b587c6fbb0910305a9e04ffd78a6aa568
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-02 12:57:41 +0200

    Edit AboutUs content

--- react-project2/src/pages/AboutUs.js
+++ react-project2/src/pages/AboutUs.js
@@ -3,10 +3,10 @@ const AboutUs = () => {
       <div className="AboutUs">
         <h1>About Us</h1>
         <div className="aboutus">
-          <h4>This is an Educational Project implemented using Laravel and React Frameworks undergone using the MVC (Model-View-Controller) software design architecural pattern.</h4>
+          <h4>This is an Educational Project implemented using Laravel Framework and React Js Library undergone using the MVC (Model-View-Controller) software design architecural pattern.</h4>
           <h4>For the Backend,PHP language was used in a Laravel Framework to implement the APIs and connect to the Database.</h4>
           <h4>The Backend represents the Model and Controller parts of this Educational Project .</h4>
-          <h4>For the Frontend, JSX language was used in a React Framework to connect with the Backend and display the returned output from the Model to the user.</h4>
+          <h4>For the Frontend, JSX language was used in a React Js Library to connect with the Backend and display the returned output from the Model to the user.</h4>
           <h4>The Frontend represents the View part of this Educational Project .</h4>
         </div>
       </div>
