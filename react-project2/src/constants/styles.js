9be033049fa8d09fd2cc460bb7309639e33adc4f
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-02 18:23:09 +0200

    ADD export constants for the project

---
+++ react-project2/src/constants/styles.js
@@ -0,0 +1,8 @@
+export const FILL_COLOR = 'wheat';
+export const BUTTON_COLOR = '#333';
+export const BUTTON_RADIUS = '10px';
+export const TEXT_COLOR = '#333';
+export const TEXTFIELD_COLOR = 'white';
+export const TEXTFIELD_RADIUS = '5px';
+export const TEXTFIELD_MARGIN = '10px';
+
