c9f93d18339f36d2375e273e18be16814e5102fe
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-03-03 02:06:38 +0200

    Clean Code

--- laravel-project/app/Http/Controllers/AuthController.php
+++ laravel-project/app/Http/Controllers/AuthController.php
@@ -124,7 +124,7 @@ class AuthController extends Controller
                 ));
         return response()->json([
             'message' => 'Data successfully Updated',
-            'user' => auth()->user()
         ], 201);
     }
 }
+    
